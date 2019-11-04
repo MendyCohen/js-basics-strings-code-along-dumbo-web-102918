141e4d3beb54a40be1d2d8fada74df6fea95c733
Mendy Cohen <mendybeltcohen@gmail.com>
2019-11-04 06:09:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // Write your code in this file
 const currentUser = "Mendy";
-let welcomeMessage = currentUser + " Welcome to Flatbook, yes!"
+let welcomeMessage = currentUser +
+" Welcome to Flatbook, yes!"
+let excitedWelcomeMessage = currentUser +
+" WELCOME TO FLATBOOK, yes!"
